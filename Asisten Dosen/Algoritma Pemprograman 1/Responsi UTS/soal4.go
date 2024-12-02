94b49f39e43f750342dba9d0f38fe1b5b04febec
Cyrus <80195151+CyrusSE@users.noreply.github.com>
2024-12-02 18:33:24 +0700

    Responsi Materi UTS Algoritma Pemprograman 1

---
+++ Asisten Dosen/Algoritma Pemprograman 1/Responsi UTS/soal4.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main(){
+	var n, total int
+	fmt.Scan(&n)
+	for i := 1; i <= n; i+2{
+		total = total + 5000
+	}
+	fmt.Println(total)
+}
+x^
+x * x * x 
+
+n = 5
+
+loop 1 
+total = 5000
+1 <= 5 = true
+loop 3
+total = 10000
+3 <= 5 = true
+loop 5
+total = 15000
+5 <= 5 = true
+loop 7
+7 <= 5 = false
